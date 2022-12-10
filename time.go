dc775dd67beebb99d9c56c7aa08c9c8031448d94
Reagan James <reaganjameskirby@gmail.com>
2022-12-10 00:02:10 -0600

    Wrote receiver function to return payload

--- time.go
+++ time.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
@@ -13,10 +14,11 @@ type Payload struct {
 var p = Payload{Message: "Automate all the things!", Time: time.Now()}
 
 func fetchTime(c *gin.Context) {
-
+	c.IndentedJSON(http.StatusOK, p)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/time", fetchTime)
+	router.Run("localhost:8080")
 }
