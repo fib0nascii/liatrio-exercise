67ba384607f631fc8c6b5916186d6623cb97c22b
Reagan James <reaganjameskirby@gmail.com>
2022-12-13 06:34:56 -0600

    Removed usage portion from script entirely

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,6 @@
 
 echo "*** Begin Deployment ***"
 
-#usage() {
-#  echo "(AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY) to be set"
-#  exit 1
-#}
-#
-#if [[ ${#} -ne 2 ]] ; then
-#  usage
-#fi
-
 echo "*** Launching Environment ***"
 
 cd terraform
